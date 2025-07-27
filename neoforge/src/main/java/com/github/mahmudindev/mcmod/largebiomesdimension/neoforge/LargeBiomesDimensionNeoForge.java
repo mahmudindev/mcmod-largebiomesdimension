e2734153496e0e42a34d8bac935ea39967bf8c3b
M. Mahmudin <42290713+mahmudindev@users.noreply.github.com>
2025-07-27 10:05:45 +0700

    Updates to 1.21

---
+++ neoforge/src/main/java/com/github/mahmudindev/mcmod/largebiomesdimension/neoforge/LargeBiomesDimensionNeoForge.java
@@ -0,0 +1,12 @@
+package com.github.mahmudindev.mcmod.largebiomesdimension.neoforge;
+
+import com.github.mahmudindev.mcmod.largebiomesdimension.LargeBiomesDimension;
+import net.neoforged.fml.common.Mod;
+
+@Mod(LargeBiomesDimension.MOD_ID)
+public final class LargeBiomesDimensionNeoForge {
+    public LargeBiomesDimensionNeoForge() {
+        // Run our common setup.
+        LargeBiomesDimension.init();
+    }
+}
